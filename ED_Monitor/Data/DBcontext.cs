d06223c6eef653bf0ed4a83f9ce5406d3230a5e4
DevN <157690726+Devan-68@users.noreply.github.com>
2025-04-21 16:35:15 +0100

    Update DBcontext.cs
    Found a bug within the file. Updated the file to the right version.

--- ED_Monitor/Data/DBcontext.cs
+++ ED_Monitor/Data/DBcontext.cs
@@ -1,17 +1,18 @@
-using Microsoft.EntityFrameworkCore;
-using ED_Monitor.Data;
+using ED_Monitor.Data.Models;
+using Microsoft.Data.SqlClient;
+using Dapper;
 
-namespace ED_Monitor.Data;
-public class ED_MonitorDbContext : DbContext
+namespace ED_Monitor.Data.Services
 {
+    public class DatabaseService
+    {
+        private readonly string _connectionString = "<your Azure connection string>";
 
-    public ED_MonitorDbContext()
-    { }
-    public ED_MonitorDbContext(DbContextOptions options) : base(options)
-    { }
-
-    // DBSet is a set of record of a specif type returned from the db
-       //  public DbSet<Air_quality> Air_quality { get; set; }
-
+        public async Task<IEnumerable<AirQualityData>> GetAirQualityDataAsync()
+        {
+            using var connection = new SqlConnection(_connectionString);
+            var query = "SELECT Date, NO2, SO2, PM25, PM10 FROM AirQualityData";
+            return await connection.QueryAsync<AirQualityData>(query);
+        }
+    }
 }
-
