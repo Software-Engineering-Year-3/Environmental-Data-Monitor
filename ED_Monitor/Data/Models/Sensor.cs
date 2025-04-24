b6c2981ec54c095f4467e4fb48db15e9cbf57c12
Distinguish Ikem <40624987@live.napier.ac.uk>
2025-04-24 20:40:35 +0100

    safety push

--- ED_Monitor/Data/Models/Sensor.cs
+++ ED_Monitor/Data/Models/Sensor.cs
@@ -1,12 +1,11 @@
-namespace ED_Monitor.Models
+namespace ED_Monitor.Models;
+
+public class Sensor
 {
-    public class Sensor
-    {
-        public int Id { get; set; }
-        public string? Name { get; set; }
-        public string? Type { get; set; } // e.g., "Air", "Water", "Weather"
-        public double Latitude { get; set; }
-        public double Longitude { get; set; }
-        public string? Status { get; set; } // e.g., "Active", "Maintenance Required"
-    }
+    public int Id { get; set; } = 0;
+    public string Name { get; set; } = string.Empty;
+    public string Type { get; set; } = string.Empty;
+    public double Latitude { get; set; }
+    public double Longitude { get; set; }
+    public string Status { get; set; } = string.Empty;
 }
